0af45e940959bbf505c1f28fbe6564f925fffc50
Nirajan <nirajanjoshi80@gmail.com>
2022-12-04 14:39:22 +0545

    Admin Auth

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,16 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+//admin
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'verified'])->name('admin.dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/adminauth.php';
